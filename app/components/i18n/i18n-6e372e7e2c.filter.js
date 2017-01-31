02a8650a28d813e4c4ddadc475aea51000ccfeaa
Nicolas Kayser-Bril <hi@nkb.fr>
2017-01-31 08:39:07 +0100

    Update 2017-01-31T07:38:59.391Z

---
+++ app/components/i18n/i18n-6e372e7e2c.filter.js
@@ -0,0 +1,2 @@
+"use strict";function i18nFilter(t){return function(i){return i[t.use()]||_(i).values().first()}}i18nFilter.$inject=["$translate"],angular.module("app").filter("i18n",i18nFilter);
+//# sourceMappingURL=../../../maps/app/components/i18n/i18n-6e372e7e2c.filter.js.map
